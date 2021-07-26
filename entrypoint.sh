53e3c1e17d2d7ab0bcbf10be0614991f195789ba
Djamel HADDADI <81908480+hddh59@users.noreply.github.com>
2021-07-26 17:16:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Hello $INPUT_MYINPUT"
-echo "Habitant $INPUT_MYINPUT"
+echo "Habitant $INPUT_MYINPUT2"
 memory=$(cat /proc/meminfo)
 cpu=$(cat /proc/cpuinfo)
 echo "::set-output name=memory::$memory"
